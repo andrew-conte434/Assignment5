6409e3b84aced86775a3c693168679cc66db6401
Andrew Conte <andrew-conte434>
2022-01-09 20:10:11 -0500

    closed issue #4 and #10

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 let cells = 1   // assuming there's at least one cell being made
-let colorList = ["#000000",
-    "#000080",
-    "#00008B",
-    "#0000CD",
-    "#0000FF",
-    "#006400",
-    "#008000",
-    "#008080"]
+let colorList = ["white",
+    "gray",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "violet",
+    "pink"]
 
 
     let colorOption = document.querySelector(".color-options")
     let grid = document.querySelector("#grid")
-    let choosenColor = document.querySelector(".color-options")
+    let chosenColor = document.querySelector(".color-options")
 
 
     let defaultColor = "gray"
 
+    // Select color from dropdown menu of colors
     function colorPick () {
-        defaultColor = choosenColor.value
+        defaultColor = chosenColor.value
     }
 
 
@@ -47,67 +50,12 @@ let colorList = ["#000000",
     
 
     function changeColor() {
-    this.style.backgroundColor = defaultColor;
-    this.classList.remove("uncolored")
-    }
-
-    function createCell(className) {
-        let cell = document.createElement("td")
-        cell.classList.add(className)
-        cell.classList.add("uncolored")
-        cell.addEventListener("click", changeColor)
-        return cell;
+        this.style.backgroundColor = defaultColor;
+        this.classList.remove("uncolored")
     }
 
 
-    // add rows
-    function addRow () {
-        cells = 1
-        let newRow = document.createElement("tr")
-        newRow.classList.add("grid-row")
-        grid.appendChild(newRow)
-
-        for (let i = 0; i < cells; i++) {
-            newRow.appendChild(createCell("grid-cell"))
-        }
-        
-    }
-
-    function addColumn () {
-        cells++ // when we add a column, we are using the row numbers to fill in the whole column
-        let rows = grid.getElementsByClassName("grid-row")
-
-        for (let i = 0; i < rows.length; i++) {
-            rows[i].appendChild(createCell("grid-cell"))
-        }
-    }
 
 
-    function deleteRow () {
-        cells--
-        let rows = grid.getElementsByClassName("grid-row")
-        let lastRow = grid.rows.length - 1
-
-        for (let i = lastRow; i > lastRow - 1; i--) {
-            grid.deleteRow(grid.rows[i])
-        }
-    }
-
-
-    function deleteColumn () {
-        cells = 1;
-        let grid = document.getElementById("grid")
-        let rows = grid.getElementsByClassName("grid-row")
-        let lastColumn = grid.rows[0].cells.length - 1
-
-        console.log(lastColumn)
-        console.log(grid.rows.length)
-    
-        for (let i = 0; i < grid.rows.length; i++) {
-            for (let j = lastColumn; j > lastColumn - 1; j--) {
-                grid.rows[i].deleteCell(j);
-            }
-        }
-    }
 
     
